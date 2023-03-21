7a7da17daa47d853d38a66aa8e1d34ef665ff5c9
unknown <nathalia.mota@etec.sp.gov.br>
2023-03-21 20:00:02 -0300

    changed tab sizes and added Aluno label

--- Registro Escolar/Form2.Designer.cs
+++ Registro Escolar/Form2.Designer.cs
@@ -29,12 +29,33 @@ namespace Registro_Escolar
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cadastro de Aluno";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(568, 292);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
--- Registro Escolar/Form3.Designer.cs
+++ Registro Escolar/Form3.Designer.cs
@@ -29,10 +29,17 @@ namespace Registro_Escolar
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(568, 292);
+            this.Name = "Form3";
             this.Text = "Form3";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
